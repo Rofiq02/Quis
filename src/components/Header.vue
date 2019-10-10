b4df5db6a7782461a9e55da6505163664a63a8a0
Rofiq02 <abdulrafiqyusufa84@gmail.com>
2019-10-10 22:21:13 +0700

    Quis dengan Vue js

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <!-- Navbar -->
+   <div>
+    <b-nav tabs>
+        <b-nav-item disabled><b>Quis App</b></b-nav-item>
+        <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+    </b-nav>
+   </div>
+</template>
+
+<script>
+export default {
+    props: [
+        'numCorrect',
+        'numTotal'
+    ]
+}
+</script>
